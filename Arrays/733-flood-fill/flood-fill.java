64e31ca196db8e766ec5a209a1e69dc5ddba7682
ntsvg <ntsvgayathrii@gmail.com>
2026-08-17 14:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (62.34%) - LeetSync

---
+++ Arrays/733-flood-fill/flood-fill.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int m = image.length;
+        int n = image[0].length;
+        int originalColor = image[sr][sc];
+        if(originalColor == color){
+            return image;
+        }
+        dfs(image,sr,sc,originalColor,color);
+        return image;
+    }
+        public void dfs(int [][] image,int i, int j,int originalColor,int color){
+            if(i < 0 || i >= image.length || j<0 || j>=image[0].length || image[i][j] != originalColor){
+                return;
+            }
+            image[i][j] = color;
+            dfs(image,i-1,j,originalColor,color);
+            dfs(image,i,j-1,originalColor,color);                        dfs(image,i,j+1,originalColor,color);
+            dfs(image,i+1,j,originalColor,color);
+
+
+        }
+}
