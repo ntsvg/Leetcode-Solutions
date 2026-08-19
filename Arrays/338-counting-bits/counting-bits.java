ed40b73a56f4e673cbae5ef8d44bc6a62b1a60cc
ntsvg <ntsvgayathrii@gmail.com>
2026-08-19 12:44:04 +0530

    Time: 2 ms (95.88%) | Memory: 48.7 MB (32.09%) - LeetSync

--- Arrays/338-counting-bits/counting-bits.java
+++ Arrays/338-counting-bits/counting-bits.java
@@ -2,16 +2,13 @@ class Solution {
     public int[] countBits(int n) {
         int [] count = new int[n+1];
         for(int i = 1;i<n+1;i++){
-            count[i] = solve(i);
+            if(i%2 == 0){
+                count[i] = count[i/2];
+            }
+            else{
+                count[i] = count[i/2]+1;
+            }
         }
         return count;
-    }
-    public static int solve(int n){
-        int c = 0;
-        while(n>0){
-            n = n&n-1;
-            c++;
-        }
-        return c;
     }
 }
